cd51cd6e3c18a5d33337b2aa7d306e991263c8bb
Josue Figueroa <poor.rican@pm.me>
2023-09-15 00:29:55 -0700

    Refactored `bot`

---
+++ bot.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+from FeedCog import FeedCog
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+# Initialize Discord client and RSS feed URL
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+    await bot.add_cog(FeedCog(bot))
--- main.py
+++ main.py
@@ -1,45 +1,12 @@
-import os
 import discord
-import feedparser
-import asyncio
+import os
 
-from discord import Intents
-from markdownify import markdownify
-from discord.ext import commands
-
-from FeedCog import FeedCog
+from bot import bot
 
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN') or ""
 if DISCORD_TOKEN == "":
     raise Exception("Please add your token to the environment variables.")
 
-intents = discord.Intents.default()
-intents.message_content = True
-
-# Initialize Discord client and RSS feed URL
-bot = commands.Bot(command_prefix='!', intents=intents)
-
-
-@bot.event
-async def on_ready():
-    print(f'Logged in as {bot.user}')
-    await bot.add_cog(FeedCog(bot))
-
-
-@bot.command('hey', aliases=['pizza', 'punk'])
-async def hey(ctx):
-    await ctx.send("Do jOO waNn_a PizZa me??")
-    await asyncio.sleep(3)
-    await ctx.send("That's what I thought...")
-    await asyncio.sleep(3)
-    await ctx.send("but waaait... I'm just a bot")
-    await asyncio.sleep(1)
-    await ctx.send("...so like, I can't eat pizza...")
-
-
-@bot.command('echo')
-async def echo(ctx):
-    await ctx.send("*echo*... *echo*... *echo*...")
 
 # Start the Discord client
 if __name__ == '__main__':
