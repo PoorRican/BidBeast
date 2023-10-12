c153bcb0882d9fbbf06f19c777abcc85931a6987
Josue Figueroa <poor.rican@pm.me>
2023-10-12 00:38:14 -0700

    Fix bug with sub-command aliases

--- cogs/MainCog.py
+++ cogs/MainCog.py
@@ -57,49 +57,49 @@ class MainCog(commands.Cog):
             await ctx.send("No sub-command given. Use `!help feed` to learn about available commands.")
 
     @feed.command('add',
-                  aliases='a',
+                  aliases=['a'],
                   help='add RSS URL to poll group')
     async def add_search(self, ctx, *args):
         await self.job_feed.searches.add_url(args[0])
 
     @feed.command('remove',
-                  aliases='r',
+                  aliases=['r'],
                   help='remove RSS URL from poll group')
     async def remove_search(self, ctx, *args):
         await ctx.send("This function has not been implemented yet")
 
     @feed.command('searches',
-                  aliases='s',
+                  aliases=['s'],
                   help='list current searches (RSS URLs)')
     async def list_searches(self, ctx):
         await ctx.send(f"Current searches:\n{self.job_feed.searches.feed_urls}")
 
     @feed.command('enable',
-                  aliases='e',
+                  aliases=['e'],
                   help='enable background loop to look for and notify about relevant jobs')
     async def enable_feed(self, _: commands.Context):
         await self.job_feed.enable_loop()
 
     @feed.command('disable',
-                  aliases='d',
+                  aliases=['d'],
                   help='disable background loop to look for and notify about relevant jobs')
     async def disable_feed(self, _: commands.Context):
         await self.job_feed.disable_loop()
 
     @feed.command('status',
-                  aliases='st',
+                  aliases=['st'],
                   help='return status of background loop')
     async def feed_status(self, ctx):
         await self.job_feed.status(ctx)
 
     @feed.command('list',
-                  aliaseses='l',
+                  aliases=['l'],
                   help='list all relevant jobs stored in cache')
     async def list_feed(self, ctx):
         await self.job_feed.list_cache()
 
     @feed.command('clear',
-                  aliases='c',
+                  aliases=['c'],
                   help='clear current cache of relevant jobs')
     async def clear_feed(self, ctx):
         await self.job_feed.clear_cache()
@@ -117,37 +117,37 @@ class MainCog(commands.Cog):
             await ctx.send("No sub-command given. Use `!help feed` to learn about available commands.")
 
     @feedback.command('process',
-                      aliases='p',
+                      aliases=['p'],
                       help='begin to process feedback for jobs that could not be automatically evaluated')
     async def process_feedback(self, ctx):
         await self.feedback.begin()
 
     @feedback.command('quit',
-                      aliases='q',
+                      aliases=['q'],
                       help='quit from processing feedback')
     async def quit_feedback(self, ctx):
         await self.feedback.exit_conversation()
 
     @feedback.command('enable',
-                      aliases='e',
+                      aliases=['e'],
                       help='enable background loop for processing feedback')
     async def enable_feedback(self, ctx):
         await self.feedback.exit_conversation()
 
     @feedback.command('disable',
-                      aliases='d',
+                      aliases=['d'],
                       help='disable background loop for processing feedback')
     async def disable_feedback(self, ctx):
         await self.feedback.exit_conversation()
 
     @feedback.command('status',
-                      aliases='s',
+                      aliases=['s'],
                       help='return status of background feedback notification task')
     async def fetch_feedback(self, ctx):
         await self.feedback.status()
 
     @feedback.command('fetch',
-                      aliases='f',
+                      aliases=['f'],
                       help='update local cache of ambiguous jobs that require feedback')
     async def feedback_status(self, ctx):
         await self.feedback.status()
