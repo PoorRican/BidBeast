60028cd90779cfe3134be7d99edc14449a1f73c6
Josue Figueroa <poor.rican@pm.me>
2024-05-11 17:41:29 -0700

    Old: Create function to regenerate summaries for all jobs

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+from asyncio import gather
+
+from functors.Summarizer import Summarizer
 from models import Job
 from functors.EmbeddingManager import EmbeddingManager
 
@@ -8,3 +11,23 @@ def global_generate_embeddings():
     jobs = results.data
 
     EmbeddingManager.generate_embeddings(jobs)
+
+
+async def global_summarization():
+    """ Generate summaries for all jobs in db """
+    async def re_summarize(row: dict):
+        summary = await Summarizer.chain.arun({'description': row['description']})
+        Job.table.update({'summary': summary}).eq('id', row['id']).execute()
+
+    results = Job.table.select('id, desc').execute()
+    _jobs = results.data
+    jobs = []
+    for job in _jobs:
+        jobs.append({'id': job['id'], 'description': job['desc']})
+
+    print(f"Fetched {len(jobs)} rows")
+
+    routines = []
+    for job in jobs:
+        routines.append(re_summarize(job))
+    await gather(*routines)
